c9e0d595f488b28050932966d1b2c379d0e2d76a
Marty <martbarc@gmail.com>
2020-07-05 23:35:36 -0400

    F

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,5 @@
 set -e
 
 echo "{$DOCKER_LOGIN}"
-./gradlew -p ./monolithic/ui packageToContainer --debug
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/web1066-the-app:latest
 docker push $DOCKER_LOGIN/web1066-the-app:latest
