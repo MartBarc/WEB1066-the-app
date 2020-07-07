2b5c5dbc905ed7e08498d7e845b95e76d710d80c
Marty <martbarc@gmail.com>
2020-07-07 00:49:35 -0400

    F

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-echo "{$DOCKER_LOGIN}"
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/web1066-the-app:latest
 docker push $DOCKER_LOGIN/web1066-the-app:latest
