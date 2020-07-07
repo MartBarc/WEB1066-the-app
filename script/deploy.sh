07af8057e91fe343bbb823fd2bb23e28be322731
Marty <martbarc@gmail.com>
2020-07-07 01:03:55 -0400

    Fixed deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,6 @@
 set -e
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin"
 
+docker pull zutherb/monolithic-shop
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/web1066-the-app:latest
 docker push $DOCKER_LOGIN/web1066-the-app:latest
